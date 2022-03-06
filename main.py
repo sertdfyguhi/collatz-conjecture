2b2f94878d941bb3e55034e83f4562f5eb03ad8b
sertdfyguhi-git <gdkamtalan@gmail.com>
2022-03-06 15:15:50 +0800

    Ok

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from threexplusone import show
+from collatz_conjecture import show
 from random import randint
 
-show([randint(10, 10001) for i in range(6)], outfile='figure.png')
+show([randint(10, 10001) for _ in range(11)], outfile='figure.png', text=False, single=True)
